627fb20e14eb7ad05e9bbd8e0341df01325fb94b
svg-den <sundaywanjiru@gmail.com>
2024-03-01 15:57:25 +0300

    done assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, words):
+        return [word for word in words if self.is_anagram(word)]
+
+    def is_anagram(self, word):
+        if len(word) != len(self.word):
+            return False
+        return sorted(word.lower()) == self.sorted_word
